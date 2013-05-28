6fb2ce6c04db188ef58332d62e70b58984dd493f
d11wtq <chris@w3style.co.uk>
2013-05-29 01:59:35 +1000

    Port existing functions from fiplr.

---
+++ grizzl-core.el
@@ -0,0 +1,169 @@
+;;; grizzl-core.el --- Fast fuzzy search index for Emacs.
+
+;; Copyright © 2013 Chris Corbyn
+;;
+;; Author:   Chris Corbyn <chris@w3style.co.uk>
+;; URL:      https://github.com/d11wtq/grizzl
+;; Version:  0.1.0
+;; Keywords: convenience, usability
+
+;; This file is NOT part of GNU Emacs.
+
+;;; --- License
+
+;; Licensed under the same terms as Emacs.
+
+;;; --- Commentary
+
+;; Overview:
+;;
+;; Grizzl provides  a fuzzy  completion framework for  general purpose
+;; use in Emacs Lisp projects.
+;;
+;; grizzl-core.el provides  the underlying data structures  and sesrch
+;; algorithm without any  UI attachment.  At the core,  a fuzzy search
+;; index is created from a list of strings, using `grizzl-make-index'.
+;; A fuzzy search  term is then used  to get a result  from this index
+;; with  `grizzl-search'.  Because  grizzl  considers the  usage of  a
+;; fuzzy  search index  to operate  in real-time  as a  user enters  a
+;; search term in the minibuffer, the framework optimizes for this use
+;; case.  Any result can be passed back into `grizzl-search' as a hint
+;; to continue searching.  The search  algorithm is able to understand
+;; insertions and deletions and therefore  minimizes the work it needs
+;; to do in this  case.  The intended use here is  to collect a result
+;; on each key press and feed that result into the search for the next
+;; key press. Once a search is  complete, the matched strings are then
+;; read, using `grizzl-result-strings'. The results are ordered on the
+;; Levenshtein distance between them and the search term.
+;;
+;; It  is assumed  that  the  index will  be  re-used across  multiple
+;; searches on larger sets of data.
+;;
+;; A discussion of the algorithm can be found in the docs/ directory.
+;;
+
+(require 'cl)
+
+;;; --- Public Functions
+
+;;;###autoload
+(defun grizzl-make-index (strings)
+  "Makes an index from the list STRINGS for use with `grizzl-search'."
+  (let ((lookup-table (make-hash-table)))
+    (reduce (lambda (list-offset str)
+              (grizzl-index-insert str list-offset lookup-table)
+              (1+ list-offset))
+            strings
+            :initial-value 0)
+    (maphash (lambda (char str-map)
+               (maphash (lambda (list-offset locations)
+                          (puthash list-offset (reverse locations) str-map))
+                        str-map)) lookup-table)
+    (cons (vconcat strings) lookup-table)))
+
+;;;###autoload
+(defun grizzl-search (term index old-result)
+  "Fuzzy searches for TERM in INDEX prepared with `grizzl-make-index'.
+If OLD-RESULT is non-nil, it is an existing search result to increment from.
+The result can be read with `grizzl-result-strings'."
+  (let* ((result (grizzl-reset-result term old-result))
+         (matches (grizzl-result-matches result))
+         (from-pos (length (grizzl-result-term result)))
+         (remainder (substring term from-pos))
+         (table (grizzl-lookup-table index)))
+    (reduce (lambda (n ch)
+              (let ((sub-table (gethash ch table)))
+                (if (not sub-table)
+                    (clrhash matches)
+                  (if (> n 0)
+                      (grizzl-search-continue sub-table matches)
+                    (grizzl-search-init sub-table matches)))
+                (1+ n)))
+            remainder
+            :initial-value from-pos)
+    (grizzl-make-result term matches)))
+
+;;;###autoload
+(defun grizzl-result-strings (result index)
+  "Returns the ordered list of matched strings in RESULT, using INDEX."
+  (let* ((matches (grizzl-result-matches result))
+         (strings (grizzl-index-strings index))
+         (loaded '()))
+    (maphash (lambda (string-offset char-offset)
+               (push (elt strings string-offset) loaded))
+             matches)
+    loaded))
+
+;;; --- Private Functions
+
+(defun grizzl-make-result (term matches)
+  "Build a new result for string TERM and hash-table MATCHES."
+    (cons term matches))
+
+(defun grizzl-reset-result (term result)
+  "Adjusts RESULT according to TERM, ready for a new search."
+  (if result
+      (let* ((old-term (grizzl-result-term result))
+             (new-len (length term))
+             (old-len (length old-term)))
+        (if (and (> new-len old-len)
+                 (string-equal old-term (substring term 0 old-len)))
+              result
+          (grizzl-make-result "" (make-hash-table))))
+    (grizzl-make-result "" (make-hash-table))))
+
+(defun grizzl-result-term (result)
+  "Returns the search term used to find the matches in RESULT."
+  (car result))
+
+(defun grizzl-result-matches (result)
+  "Returns the internal hash used to track the matches in RESULT."
+  (cdr result))
+
+(defun grizzl-result-count (result)
+  "Returns the number of matches present in RESULT."
+  (hash-table-count (grizzl-result-matches result)))
+
+(defun grizzl-index-insert (string list-offset index)
+  "Inserts STRING at LIST-OFFSET into INDEX."
+  (reduce (lambda (char-offset char)
+            (let* ((str-map (or (gethash char index)
+                                (puthash char (make-hash-table) index)))
+                   (offsets (gethash list-offset str-map)))
+              (puthash list-offset
+                       (cons char-offset offsets)
+                       str-map)
+              (1+ char-offset)))
+          string
+          :initial-value 0))
+
+(defun grizzl-lookup-table (index)
+  "Returns the lookup table portion of INDEX."
+  (cdr index))
+
+(defun grizzl-index-strings (index)
+  "Returns the vector of strings stored in INDEX."
+  (car index))
+
+(defun grizzl-search-init (sub-table result)
+  "Initializes potential matches if the first char matched in search."
+  (maphash (lambda (k v)
+             (puthash k (car v) result))
+           sub-table))
+
+(defun grizzl-search-continue (sub-table result)
+  "Use the search lookup table to filter already-accumulated results."
+  (cl-flet ((next-offset (key current sub-table)
+              (find-if (lambda (v)
+                         (> v current))
+                       (gethash key sub-table))))
+    (maphash (lambda (k v)
+               (let ((offset (next-offset k v sub-table)))
+                 (if offset
+                     (puthash k offset result)
+                   (remhash k result))))
+             result)))
+
+(provide 'grizzl-core)
+
+;;; grizzl-core.el ends here
---
+++ test/grizzl-core-test.el
@@ -0,0 +1,41 @@
+;;; grizzl-core-test.el --- Fuzzy search index tests.
+
+;; Copyright © 2013 Chris Corbyn
+
+;;; --- Unit Tests
+
+(load (expand-file-name "../grizzl-core.el"))
+
+(require 'grizzl-core)
+
+(ert-deftest index-search-test ()
+  "Test grizzl can index and fuzzy-search a list of strings."
+  (let* ((strings '("models" "controllers" "views"))
+         (index (grizzl-make-index strings)))
+    (cl-flet ((search (term)
+                (let ((result (grizzl-search term index nil)))
+                  (grizzl-result-strings result index))))
+      (should (equal (search "oe")  '("controllers" "models")))
+      (should (equal (search "iw")  '("views")))
+      (should (equal (search "bad") '()))
+      (should (equal (search "es")  '("views" "controllers" "models"))))))
+
+(ert-deftest increment-search-append-test ()
+  "Test grizzl can accept an existing result and search string to search."
+  (let* ((strings '("models" "controllers" "views"))
+         (index (grizzl-make-index strings))
+         (result (grizzl-search "ol" index nil))
+         (matches (grizzl-result-strings (grizzl-search "olr" index result)
+                                     index)))
+    (should (equal matches  '("controllers")))))
+
+(ert-deftest increment-search-deletion-test ()
+  "Test grizzl handles repeated search when backspace was hit."
+  (let* ((strings '("models" "controllers" "views"))
+         (index (grizzl-make-index strings))
+         (result (grizzl-search "olr" index nil))
+         (matches (grizzl-result-strings (grizzl-search "ol" index result)
+                                     index)))
+    (should (equal matches  '("controllers" "models")))))
+
+;; TODO: Implement and test Levenshtein ordering
